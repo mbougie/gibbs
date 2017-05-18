901d51229a522e355c8ff7ac4804c126096bd181
Matt Bougie <mbougie@wisc.edu>
2017-05-17 22:15:56 -0500

    deliverable.py----reformated and refined question_b code

--- processes/deliverables.py
+++ processes/deliverables.py
@@ -17,15 +17,15 @@ import psycopg2
 
 
 
-# try:
-#     conn = psycopg2.connect("dbname='delivery' user='postgres' host='localhost' password='postgres'")
-# except:
-#     print "I am unable to connect to the database"
+try:
+    conn = psycopg2.connect("dbname='usxp' user='mbougie' host='144.92.235.105' password='Mend0ta!'")
+except:
+    print "I am unable to connect to the database"
 
 
 
 arcpy.CheckOutExtension("Spatial")
-case=['bougie','gibbs']
+case=['Bougie','Gibbs']
 
 
 ###################  declare functions  #######################################################
@@ -103,35 +103,38 @@ def question_a4():
 
 
 
-def createGSconv(wc,fn):
+def createGSconv(gdb_path,wc,outfile):
+	#DESCRIPTION: subset bfc datasets to grass and shrub
+	arcpy.env.workspace = defineGDBpath(gdb_path)
 
-	raster = Raster(wc)
+	for raster in arcpy.ListDatasets(wc, "Raster"): 
+		
+		print 'raster: ',raster
 
-	print 'raster: ',raster
+		# Set the cell size environment using a raster dataset.
+		arcpy.env.cellSize = raster
 
-	# Set the cell size environment using a raster dataset.
-	arcpy.env.cellSize = raster
+		# Set Snap Raster environment
+		arcpy.env.snapRaster = raster
 
-	# Set Snap Raster environment
-	arcpy.env.snapRaster = raster
+		output = defineGDBpath(['deliverables','deliverables'])+outfile
+		print 'output: ', output
 
-	output = 'C:/Users/bougie/Desktop/'+rootDir+'/usxp/'+gdb+'/'+fn
-	print 'output: ', output
+		cond='Value = 37 OR Value =62 OR Value = 64 OR Value =152 OR Value =171 OR Value =181 OR Value =176'
+		print 'cond', cond
 
-	cond='Value = 37 OR Value =62 OR Value = 64 OR Value =152 OR Value =171 OR Value =181 OR Value =176'
-	print 'cond', cond
+		attExtract = ExtractByAttributes(raster, cond) 
 
-	attExtract = ExtractByAttributes(raster, cond) 
-
-	# Save the output 
-	attExtract.save(output)
+		# Save the output 
+		attExtract.save(output)
 
 
 
 
 def createGSconvBylcc(wc):
+	# DESCRIPTION: replace the gsconv value with lcc value
 
-	arcpy.env.workspace = 'C:/Users/bougie/Desktop/'+rootDir+'/usxp/'+gdb
+	arcpy.env.workspace = defineGDBpath(['deliverables','deliverables'])
 
 	print 'wc: ', wc
 
@@ -161,9 +164,9 @@ def createGSconvBylcc(wc):
 
 
 
-def createGSconvByYearANDlcc(wc,yr_dset,years):
+def createGSconvByYearANDlcc(wc,years):
 
-	arcpy.env.workspace = 'C:/Users/bougie/Desktop/'+rootDir+'/usxp/'+gdb
+	arcpy.env.workspace = defineGDBpath(['deliverables','deliverables_refined'])
 
 	print 'wc: ', wc
 
@@ -178,16 +181,18 @@ def createGSconvByYearANDlcc(wc,yr_dset,years):
 		arcpy.env.snapRaster = raster
 
         #set up the 2 datasets that will be used in the Con() function below
-		# yr_dset='C:/Users/bougie/Desktop/'+rootDir+'/usxp/'+gdb+'/'+fname
-		lcc='C:/Users/bougie/Desktop/'+rootDir+'/usxp/ancillary.gdb/lcc_100m_reproject'
+		yr_dset=defineGDBpath(['deliverables','xp_update_refined'])+'ytc'
+		lcc=defineGDBpath(['ancillary','other'])+'LCC_100m'
 
 		
 		for year in years:
-			output = 'C:/Users/bougie/Desktop/'+rootDir+'/usxp/'+gdb+'/gsConv_'+year+'_lcc'
+			output = 'gsConv_'+year+'_lcc'
 			print 'output: ', output
 
+			print year[2:]
+
 			# using 3 rasters in this condtion. only select pixels of year x, if true get the lcc value for that pixel if fale set to null!!
-			OutRas=Con((Raster(yr_dset) == int(year)), Raster(lcc),(SetNull(raster, raster,  "Value > 8")))
+			OutRas=Con((Raster(yr_dset) == int(year[2:])), Raster(lcc),(SetNull(raster, raster,  "Value > 8")))
 			# OutRas=Con((Raster(yr_dset) == int(year)), Raster(lcc),raster)
 
 			#Save the output 
@@ -197,9 +202,9 @@ def createGSconvByYearANDlcc(wc,yr_dset,years):
 
 
 
-def tabArea(wc):
+def tabAreaByCounty(wc):
 	# Set environment settings
-	arcpy.env.workspace = 'C:/Users/bougie/Desktop/'+rootDir+'/usxp/'+gdb
+	arcpy.env.workspace = defineGDBpath(['deliverables','deliverables_refined'])
     
 	# Set local variables
 	for raster in arcpy.ListDatasets(wc, "Raster"): 
@@ -212,11 +217,11 @@ def tabArea(wc):
 		arcpy.env.snapRaster = raster
 
 
-		inZoneData = 'C:/Users/bougie/Desktop/'+rootDir+'/usxp/ancillary.gdb/counties'
+		inZoneData = defineGDBpath(['ancillary','shapefiles'])+'counties'
 		zoneField = "atlas_stco"
 		inClassData = raster
 		classField = "Value"
-		outTable = inClassData + '_ta'
+		outTable = inClassData + '_counties'
 
 		#get the resolution of each raster to get the coorect size to process
 		res = arcpy.GetRasterProperties_management(raster, "CELLSIZEX")
@@ -230,147 +235,148 @@ def tabArea(wc):
 		TabulateArea(inZoneData, zoneField, inClassData, classField, outTable,processingCellSize)
 
 
-
-
-def createTables_ta1(wc):
-	arcpy.env.workspace = 'C:/Users/bougie/Desktop/'+rootDir+'/usxp/'+gdb
+def createPGTables_lcc(wc):
+	arcpy.env.workspace = defineGDBpath(['deliverables','deliverables_refined'])
 
 	for table in arcpy.ListTables(wc): 
 		print 'table: ', table
-		yo = str(table)
-		print yo
-		print type(yo[0])
-		y='this_is_a_string'
-		fnf=(os.path.splitext(str(table))[0]).split("_")
-		print fnf
-
-
-        #define variables
-		schema = fnf[0]
-		print 'schema: ', schema
-		year = fnf[1]
-		tablename = schema.lower()+'.'+str(table)
+	    
+	    #define table and column names
+		tablename = 'deliverables.'+table
 		print 'tablename', tablename
-		columnname = str(table)[:-4]
+		columnname = table.replace('_counties','')
 		print columnname
 
 		cur = conn.cursor()
 		query='CREATE TABLE '+tablename+'(atlas_stco text, ' + columnname + '12 integer, ' + columnname + '34 integer, '+columnname+ '56 integer, '+columnname+ '78 integer)'
-		# query='CREATE TABLE '+tablename+'(atlas_stco text, ' + columnname + '12 integer, ' + columnname + '34 integer, '+columnname+ '56 integer)'
+
 		print query
 		cur.execute(query)
 		conn.commit()
+     
+        #loop through each row and get the value for specified columns
+		rows = arcpy.SearchCursor(table)
+		for row in rows:
 
-		with arcpy.da.SearchCursor(table, "*") as cur:
-			for row in cur:
-				print row
-				print row[2]
-
-				atlas_stco = row[1]
-				lcc12 = str((row[3]+row[4])*(0.000247105))
-				lcc34 = str((row[5]+row[6])*(0.000247105))
-				lcc56 = str((row[7]+row[8])*(0.000247105))
-				lcc78 = str((row[9]+row[10])*(0.000247105))
+			atlas_stco = row.getValue("ATLAS_STCO")
+			lcc12 = str((row.getValue('VALUE_1')+row.getValue('VALUE_2'))*(0.000247105))
+			lcc34 = str((row.getValue('VALUE_3')+row.getValue('VALUE_4'))*(0.000247105))
+			lcc56 = str((row.getValue('VALUE_5')+row.getValue('VALUE_6'))*(0.000247105))
+			lcc78 = str((row.getValue('VALUE_7')+row.getValue('VALUE_8'))*(0.000247105))
 			
 
-				cur = conn.cursor()
-				query="INSERT INTO "+tablename+" VALUES ('" + atlas_stco + "' , " + lcc12 + " , " + lcc34 + " , " + lcc56+ " , " + lcc78 + ")"
-				print query
-				cur.execute(query)
-				conn.commit()
+			cur = conn.cursor()
+			query="INSERT INTO "+tablename+" VALUES ('" + atlas_stco + "' , " + lcc12 + " , " + lcc34 + " , " + lcc56+ " , " + lcc78 + ")"
+			print query
+			cur.execute(query)
+			conn.commit()
       
 
-
-def createTables_ta2(wc,year):
-	arcpy.env.workspace = 'C:/Users/bougie/Desktop/'+rootDir+'/usxp/'+gdb
-
+def createPGTables_mtr(year):
+	arcpy.env.workspace = defineGDBpath(['pre','binaries'])
+    wc='*'+year+'*'
 	for table in arcpy.ListTables(wc): 
 		print 'table: ', table
-		yo = str(table)
-		print yo
-		print type(yo[0])
-		y='this_is_a_string'
-		fnf=(os.path.splitext(str(table))[0]).split("_")
-		print fnf
 
-
-		#define variables
-		schema = fnf[0]
-		print 'schema: ', schema
-		# year = fnf[1]
-		# print year
-		tablename = 'totalcrop.totalcrop_'+year+'_ta2'
-		print 'tablename', tablename
+		#define table and column names
+		tablename = 'deliverables.totalcrop_'+year+'_counties'
+		print 'tablename: ', tablename
 		columnname = 'totalcrop_'+year
-		print columnname
-
+		print 'columnname: ', columnname
+        
+        #execute quesry to postgres db
 		cur = conn.cursor()
 		query='CREATE TABLE '+tablename+'(atlas_stco text, ' + columnname + ' integer)'
-		
-		print query
+		print 'query: ', query
 		cur.execute(query)
 		conn.commit()
 
-       
-        if year == '2012':
-			with arcpy.da.SearchCursor(table, "*") as cur:
-				for row in cur:
-                    #OBJECTID, ATLAS_STCO, VALUE_1, VALUE_2, VALUE_3, VALUE_4, VALUE_5
 
-					atlas_stco = row[1]
-					print (row[3]+row[6])*(0.000247105)
+        #loop through each row and get the value for specified columns
+		rows = arcpy.SearchCursor(table)
+		for row in rows:
+
+			atlas_stco=row.getValue("ATLAS_STCO")
+			count = getCount(year,row)
+
+			cur = conn.cursor()
+			query="INSERT INTO "+tablename+" VALUES ('" + atlas_stco + "' , " + count + ")"
+			print query
+			cur.execute(query)
+			conn.commit()
+
+
+       
+   #      if year == '2012':
+			# with arcpy.da.SearchCursor(table, "*") as cur:
+			# 	for row in cur:
+   #                  #OBJECTID, ATLAS_STCO, VALUE_1, VALUE_2, VALUE_3, VALUE_4, VALUE_5
+
+			# 		atlas_stco = row[1]
+			# 		print (row[3]+row[6])*(0.000247105)
 				
 
-					count = str((row[3]+row[6])*(0.000247105))
+			# 		count = str((row[3]+row[6])*(0.000247105))
 			
-					cur = conn.cursor()
-					query="INSERT INTO "+tablename+" VALUES ('" + atlas_stco + "' , " + count + ")"
-					print query
-					cur.execute(query)
-					conn.commit()
+			# 		cur = conn.cursor()
+			# 		query="INSERT INTO "+tablename+" VALUES ('" + atlas_stco + "' , " + count + ")"
+			# 		print query
+			# 		cur.execute(query)
+			# 		conn.commit()
 
-        elif year == '2015':
-			with arcpy.da.SearchCursor(table, "*") as cur:
-				for row in cur:
-					#OBJECTID, ATLAS_STCO, VALUE_1, VALUE_2, VALUE_3, VALUE_4, VALUE_5
+   #      elif year == '2015':
+			# with arcpy.da.SearchCursor(table, "*") as cur:
+			# 	for row in cur:
+			# 		#OBJECTID, ATLAS_STCO, VALUE_1, VALUE_2, VALUE_3, VALUE_4, VALUE_5
 
-					atlas_stco = row[1]
-					print ((row[3]+row[6])+(row[4]-row[5]))*(0.000247105)
-
-
-					count = str(((row[3]+row[6])+(row[4]-row[5]))*(0.000247105))
-
-					cur = conn.cursor()
-					query="INSERT INTO "+tablename+" VALUES ('" + atlas_stco + "' , " + count + ")"
-					print query
-					cur.execute(query)
-					conn.commit()
+			# 		atlas_stco = row[1]
+			# 		print ((row[3]+row[6])+(row[4]-row[5]))*(0.000247105)
 
 
+			# 		count = getCount(year,row)
+
+			# 		cur = conn.cursor()
+			# 		query="INSERT INTO "+tablename+" VALUES ('" + atlas_stco + "' , " + count + ")"
+			# 		print query
+			# 		cur.execute(query)
+			# 		conn.commit()
 
 
-      
+def getCount(year):
+	if year == '2012':
+		# count = str((row[3]+row[6])*(0.000247105))
+		count = str((row.getValue()+row.getValue())*(0.000247105))
+		return count
+
+	elif year == '2015':
+		# count = str(((row[3]+row[6])+(row[4]-row[5]))*(0.000247105))
+		count = str(((row.getValue()+row.getValue())+(row.getValue()-row.getValue()))*(0.000247105))
+		return count
 
 
 
 
 
 ###########  subset bfc datasets to grass and shrub  ################################################
-# createGSconv('C:/Users/bougie/Desktop/'+rootDir+'/'+production_type+'/rasters/post/ytc.gdb/ytc_bfc_mosaic_traj_n8h_mtr_8w_msk45_nbl_fnl','gsConv_new')
-# createGSconv('C:/Users/bougie/Desktop/'+rootDir+'/usxp/ancillary.gdb/class_before_crop','gsConv_old')
-# createGSconv('D:/gibbs/production/rasters/pre/cdl/2012_30m_cdls.img','gs2012')
+# createGSconv(['post','xp_update_refined'], 'bfc', 'gsConv_new')
+# createGSconv(['ancillary','data_2008_2012'], 'class_before_crop', 'gsConv_old')
+# createGSconv(['ancillary','cdl'], '2012_30m_cdls', 'gs2012')
+# erase this if above code works!!!!!!!       createGSconv('D:/gibbs/production/rasters/pre/cdl/2012_30m_cdls.img','gs2012')
 
 ###########  subset the gsConv datasets by year and then attach lcc value  ###########################
-# createGSconvByYearANDlcc('gsConv_new','C:/Users/bougie/Desktop/'+rootDir+'/'+production_type+'/rasters/post/ytc.gdb/ytc_b_mosaic_traj_n8h_mtr_8w_msk45_nbl_fnl',['2013','2014','2015'])
+# createGSconvByYearANDlcc('gsConv_new',['2013','2014','2015'])
 # createGSconvByYearANDlcc('gsConv_old','D:/gibbs/control/raster/2008_2012_data/ytc_ff2.tif',['2009','2010','2011','2012'])
 # createGSconvBylcc('gs2012')
 
 ###########  Execute TabulateArea  ###################################################################
-# tabArea("gs2012_lcc")
+# tabAreaByCounty("*lcc")
 
 ###########  create/populate tables in postgres  ###################################################################
-# createTables_ta1('*_lcc_ta1')
-# createTables_ta2('*2015_ta2','2015')
+createPGTables_lcc('*_lcc_counties')
+
+naive_years=['2012','2015']
+for year in naive_years:
+	createPGTables_mtr(year)
 
 
 
