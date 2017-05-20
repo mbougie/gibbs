f30d5b502207aa250307712cd349b20bafc0fb8d
Matt Bougie <mbougie@wisc.edu>
2017-05-20 17:55:23 -0500

    continued refinement of pre.py code

--- processes/pre.py
+++ processes/pre.py
@@ -14,6 +14,11 @@ from itertools import groupby
 arcpy.CheckOutExtension("Spatial")
 
 
+try:
+    conn = psycopg2.connect("dbname='usxp' user='mbougie' host='144.92.235.105' password='Mend0ta!'")
+except:
+    print "I am unable to connect to the database"
+
 
 #########  global variables   ################
 #acccounts for different machines having different cases in path
@@ -39,19 +44,19 @@ def reclassifyRaster(gdb_args, wc, reclasstable):
     for raster in arcpy.ListDatasets(wc, "Raster"): 
         
         print 'raster: ',raster
-        outraster = raster.replace("_", "_"+reclasstable+"_")
-        # #get the arc_reclassify table
+        # outraster = raster.replace("_", "_"+reclasstable+"_")
+        outraster = reclasstable + raster[-5:]
+        print 'outraster: ', outraster
+        #get the arc_reclassify table
         inRemapTable = 'C:/Users/Bougie/Desktop/Gibbs/arcgis/arc_reclassify_table/'+reclasstable
         print 'inRemapTable: ', inRemapTable
 
-        # #define the output
+        #define the output
         output = defineGDBpath(['pre','binaries'])+outraster
         print 'output: ', output
 
         #Execute Reclassify
         arcpy.gp.ReclassByTable_sa(raster,inRemapTable,"FROM","TO","OUT",output,"NODATA")
-
-
 
 
 
@@ -65,59 +70,70 @@ def createTrajectories(gdb_args,wc):
     
     #sort the rasterlist by accending years
     rasterList.sort(reverse=False)
+    
+    if wc == 'b':
+        #for the binary trajectories, moves the last element in the list (i.e. nlcd_b_2011) to the first element position in the list.
+        rasterList.insert(0, rasterList.pop())
+    
     print 'rasterList: ',rasterList
 
     #Execute Combine
     outCombine = Combine(rasterList)
     print 'outCombine: ', outCombine
     
-    output = defineGDBpath(['pre', 'trajectories'])+'traj_'+wc
+    output = defineGDBpath(['pre','trajectories'])+'traj_'+wc
+    
     #Save the output 
     outCombine.save(output)
 
 
 
-def gdbTable2postgres(dataset):
+def addGDBTable2postgres(dataset):
     #set the engine.....
-    engine = create_engine('postgresql://postgres:postgres@localhost:5432/core')
+    engine = create_engine('postgresql://mbougie:Mend0ta!@144.92.235.105:5432/usxp')
 
     #path to the table you want to import into postgres
-    input = 'C:/Users/bougie/Desktop/gibbs/production/processes/pre/pre.gdb/'+dataset
+    input = defineGDBpath(['pre','trajectories'])+dataset
 
     #populate the fields from the atribute table into argument variable
 
     # Execute AddField twice for two new fields
     fields = [f.name for f in arcpy.ListFields(input)]
 
-      
     arr = arcpy.da.TableToNumPyArray(input,fields)
     print arr
      
     df = pd.DataFrame(data=arr)
     # df["rename"] = np.nan
 
-
     print df
     
+    #use pandas method to import table into psotgres
     df.to_sql(dataset, engine, schema='pre')
 
 
 
-
-
-
-def PG_DDLandDML(degree_lc):
-    conn = psycopg2.connect(database = "core", user = "postgres", password = "postgres", host = "localhost", port = "5432")
-    print "Opened database successfully"
-
+def addTrajArrayField(degree_lc):
     cur = conn.cursor()
+    arcpy.env.workspace = defineGDBpath(['pre','binaries'])
     
-    # add column to hold arrays
-    cur.execute('ALTER TABLE pre.traj ADD COLUMN traj_array integer[];');
+    #store the rasternames on defined gdb into array
+    rasterList = arcpy.ListDatasets('*'+degree_lc+'*', "Raster")
     
-    # insert values into arrays
-    cur.execute('UPDATE pre.traj SET traj_array = ARRAY[rc_' + degree_lc + '_nlcd2,rc_' + degree_lc + '_2010,rc_' + degree_lc + '_2011,rc_' + degree_lc + '_2012,rc_' + degree_lc + '_2013,rc_' + degree_lc + '_2014,rc_' + degree_lc + '_2015,rc_' + degree_lc + '_2016];');
+    if degree_lc == 'b':
+        #for the binary trajectories, moves the last element in the list (i.e. nlcd_b_2011) to the first element position in the list.
+        rasterList.insert(0, rasterList.pop())
+    
+    #convert the rasterList into a string
+    columnList = ','.join(rasterList)
+    print columnList
 
+    # add column to hold arrays
+    cur.execute('ALTER TABLE pre.traj_' + degree_lc + ' ADD COLUMN traj_array integer[];');
+    
+    #insert values new array column
+    cur.execute('UPDATE pre.traj_' + degree_lc + ' SET traj_array = ARRAY['+columnList+'];');
+    
     conn.commit()
     print "Records created successfully";
     conn.close()
@@ -125,8 +141,6 @@ def PG_DDLandDML(degree_lc):
 
 
 def createReclassifyList(degree_lc):
-
-
     arcpy.env.workspace = 'C:/Users/bougie/Desktop/gibbs/refinement/trajectories.gdb/'
     engine = create_engine('postgresql://postgres:postgres@localhost:5432/core')
     df = pd.read_sql_query("select \"Value\",new_value from refinement.traj_"+degree_lc+" as a JOIN refinement.traj_lookup as b ON a.traj_array = b.traj_array WHERE b.name='"+degree_lc+"'",con=engine)
@@ -162,108 +176,29 @@ def mosaicRasters():
 
 
 
-def mtrAlgorithms(x):
-    # arcpy.env.workspace = 'C:/Users/bougie/Desktop/'+rootDir+'/'+production_type+'/processes/pre/pre.gdb'
-    #DESCRIPTION:subset the trajectoires by year to create binary ytc or ytc raster by year that represent the conversion to/from crop between succesive years
-    engine = create_engine('postgresql://postgres:postgres@localhost:5432/core')
-    df = pd.read_sql_query('select traj_array from pre.traj where mtr IS NULL',con=engine)
-    # print 'df--',df
 
-
-
-    # a = df.values.flatten()
-    # # a.flatten()
-
-    # # print a
-    # # print type(a)
-
-    # l=a.tolist()
-    # print l
-
-    # for x in l:
-    #   print x.count(0)
-
-    for index, row in df.iterrows():
-        
-        l=row.tolist()
-        flat=sum(l, [])
-        grouped_l = [(k, sum(1 for i in g)) for k,g in groupby(flat)]
-        length = len(grouped_l)
-        # print flat
-        # print grouped_l
-        # print type(length)
-
-        count = flat.count(x)
-
-        
-        if x == 1 or x == 2:
-            count = flat.count(x-1)
-            if count >= 7:
-                print count
-                print row
-                tryit(str(x),str(flat))
-
-        elif x == 3:
-            print 'yty'
-            if len(grouped_l) == 2 and grouped_l[0][0] == 0 and grouped_l[0][1] > 3:
-                print len(grouped_l)
-                print grouped_l
-                print count
-                print row
-                tryit(str(x),str(flat))
-            # elif len(grouped_l) == 2 and grouped_l[0][0] == 1 and grouped_l[0][1] <= 3:
-            #   x=2
-            #   tryit(str(x),str(flat))
-
-        elif x == 4:
-            print 'ywwwwwwwwwwwty'
-            if len(grouped_l) == 2 and grouped_l[0][0] == 1 and grouped_l[0][1] > 3:
-                print len(grouped_l)
-                print grouped_l
-                print count
-                print row
-                tryit(str(x),str(flat))
-                # elif len(grouped_l) == 2 and grouped_l[0][0] == 1 and grouped_l[0][1] <= 3:
-                #   x=1
-                #   tryit(str(x),str(flat))
-
-        elif x == 5:
-            if len(grouped_l) >= 3:
-                print count
-                print row
-
-                tryit(str(x),str(flat))
-
-   
-
-
-
-def tryit(x,row):
-    conn = psycopg2.connect(database = "core", user = "postgres", password = "postgres", host = "localhost", port = "5432")
-    print "Opened database successfully"
-
-    cur = conn.cursor()
-    
-    # add column to hold arrays
-    # cur.execute('ALTER TABLE pre.traj ADD COLUMN traj_array integer[];');
-    
-    # insert values into arrays
-    cur.execute('UPDATE pre.traj SET mtr = '+x+' where traj_array = array' + row);
-
-    conn.commit()
-    print "Records created successfully";
-    conn.close()
 
 
 ######  call functions  #############################
 #-----reclassifyRaster(geodatabase path arguments, wildcard, arcgis reclass table)------------------
 # reclassifyRaster(['ancillary','cdl'], "*", 'cdl_b')
-createTrajectories(['pre','binaries'],"b")
-# gdbTable2postgres('traj')
-# PG_DDLandDML('b')
+# reclassifyRaster(['ancillary','misc'], "*nlcd*", 'nlcd_b')
+
+#-----use arcgis combine() function to create permuations 
+# createTrajectories(['pre','binaries'],"b")
+
+#-----add the trajectories atribute table 
+# addGDBTable2postgres('traj_b')
+
+#-----add field to PG table
+# addTrajArrayField('b')
+
+#-----describe
 # createReclassifyList('tdev')
+
+#-----describe
 #mosaicRasters()
 
-# trylist = [1,2,3,4,5]
-# for x in trylist:
-#     mtrAlgorithms(x)
+
+
+
