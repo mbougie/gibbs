496fb87f4711a16d01b09e12f27f61f7167293c7
Matt Bougie <mbougie@wisc.edu>
2017-07-12 16:56:03 -0500

    added more functionality to clu.py script

--- processes/clu.py
+++ processes/clu.py
@@ -108,126 +108,76 @@ mmu_Raster=Raster(mmu_gdb + mmu)
 
 
 
-def createMetaTable(state):
-    os.chdir('C:/Users/Bougie/Desktop/Gibbs/Intact_land/SDSU_CLU_project/CLU Data/'+ state)
+def createMetaTable():
+    states = getDirectories()
+    for state in states:
+        if len(state) == 2:
+            #only want to process the main directories with 2 letters that represnt the to letters of a state
+            print state
+        
+            os.chdir('C:/Users/Bougie/Desktop/Gibbs/Intact_land/SDSU_CLU_project/CLU Data/'+ state)
+            
 
-    for file in glob.glob("*.shp"):
-        # print file
-        fnf=(os.path.splitext(file)[0]).split("_")
-        print fnf
+            for file in glob.glob("*.shp"):
+                # print file
+                fnf=(os.path.splitext(file)[0]).split("_")
+                print fnf
 
-        # # insert the values into
-        # insertValuesPG(fnf)
-        # #fnf=file name fragments
-        # fnf=(os.path.splitext(fc)[0]).split("_")
-        # print fnf
-
-        cur = conn.cursor()
-        query="INSERT INTO metadata.metatable VALUES (" + str(fnf[2]) + " , '" + str(fnf[4]) + "' , '" + str(fnf[5])+ "' , '" + str(file)+ "')"
-        print query
-        cur.execute(query)
-        conn.commit()
+                cur = conn.cursor()
+                query="INSERT INTO metadata.metatable VALUES ('" + state + "' , '" + str(fnf[2])  + "' , '" + str(fnf[4]) + "' , '" + str(fnf[5])+ "' , '" + str(file)+ "')"
+                print query
+                cur.execute(query)
+                conn.commit()
 
 
-def main_layers(gdb_path, wc, state, year):
+
+
+def mergeFC(gdb_path, wc, state, year):
     arcpy.env.workspace = defineGDBpath(gdb_path)
     featureclasses = arcpy.ListFeatureClasses(wc)
     print type(featureclasses)
   
     if len(featureclasses) >= 1:
  
-        arcpy.Merge_management(featureclasses, defineGDBpath(['intact_land','merged'])+state+'_'+str(year))
-
-    # arcpy.Dissolve_management(defineGDBpath(['intact_land','merged'])+'merged', defineGDBpath(['intact_land','merged'])+'merged_dissolved2', ["CLUCLSCD"], "", "SINGLE_PART", "DISSOLVE_LINES")
-
-   
-
-def createLayer(gdb_path, fc, layerlist):
+        arcpy.Merge_management(featureclasses, defineGDBpath(['intact_land',state+'_merged'])+state+'_'+str(year))
 
 
-    # Set overwrite option
-    arcpy.env.overwriteOutput = True
-
-    layer = 'layer_'+fc
-    # Make a layer from the feature class
-    arcpy.MakeFeatureLayer_management(defineGDBpath(gdb_path)+fc,layer)
-
-    # # Within the selection (done above) further select only those cities that have a population >10,000
-    arcpy.SelectLayerByAttribute_management(layer, "NEW_SELECTION", "CLUCLSCD = 2")
-
-    addLayerToMXD(layer)
-
-    layerlist.append(layer)
-
-
-    # Execute Dissolve using LANDUSE and TAXCODE as Dissolve Fields
-    # arcpy.Dissolve_management("temp_layer", defineGDBpath(['intact_land','merged'])+'try4', ["CLUCLSCD"], "", "MULTI_PART", "DISSOLVE_LINES")
-
-    # # Write the selected features to a new featureclass
-    # arcpy.CopyFeatures_management("temp_layer", defineGDBpath(['intact_land','merged'])+'try3')
-
-
-def addLayerToMXD(layer):
-    mxd = arcpy.mapping.MapDocument(r"C:/Users/Bougie/Desktop/Gibbs/arcgis/map_documents/temp_map.mxd")
-    df = arcpy.mapping.ListDataFrames(mxd)[0]
-    addlayer = arcpy.mapping.Layer(layer)
-    arcpy.mapping.AddLayer(df, addlayer, "AUTO_ARRANGE")
-
-    mxd.save()
-
-    del mxd
-
-        
-def deleteLayers():
-    mxd = arcpy.mapping.MapDocument(r"C:/Users/Bougie/Desktop/Gibbs/arcgis/map_documents/temp_map.mxd")
-    for df in arcpy.mapping.ListDataFrames(mxd):
-        for lyr in arcpy.mapping.ListLayers(mxd, "", df):
-            arcpy.mapping.RemoveLayer(df, lyr)
-    mxd.save()
-    del mxd
-
-
-
-######  call getRasterCount() function  ##############################
-
-# years = range(2005, 2017)
-# states = ['ia4']
-# for state in states:
-#     for year in years:
-#         main_layers(['intact_land',state], '*_acea_'+str(year)+'*', state, year)
-    # deleteLayers()
-
-
-# arcpy.Project_management(clu_c_19001_15_20051206_20060228_shp, clu_c_19001_15_20051206_2006, "PROJCS['Albers_Conical_Equal_Area',GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Albers'],PARAMETER['false_easting',0.0],PARAMETER['false_northing',0.0],PARAMETER['central_meridian',-96.0],PARAMETER['standard_parallel_1',29.5],PARAMETER['standard_parallel_2',45.5],PARAMETER['latitude_of_origin',23.0],UNIT['Meters',1.0]]", "", "PROJCS['NAD_1983_UTM_Zone_15N',GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Transverse_Mercator'],PARAMETER['False_Easting',500000.0],PARAMETER['False_Northing',0.0],PARAMETER['Central_Meridian',-93.0],PARAMETER['Scale_Factor',0.9996],PARAMETER['Latitude_Of_Origin',0.0],UNIT['Meter',1.0]]", "NO_PRESERVE_SHAPE", "", "NO_VERTICAL")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def ProjectSF(gdb_path, infc, outfc):
+def addFields(gdb_path, state, year): 
+    print gdb_path
+    print year
     arcpy.env.workspace = defineGDBpath(gdb_path)
 
+    featureclasses = arcpy.ListFeatureClasses(wild_card = '*'+str(year))
+
+    # Copy shapefiles to a file geodatabase
+    for fc in featureclasses:
+
+        print fc
+     
+        # Execute AddField twice for two new fields
+        # arcpy.AddField_management(fc, 'state', "TEXT")
+        
+        #fill column with uniform value
+        addValuetoColumn(fc, 'state', state)
+        
+        # arcpy.AddField_management(fc, 'year', "SHORT", field_length=5)
+
+        #fill column with uniform value
+        addValuetoColumn(fc, 'year', year)
+
+def addValuetoColumn(fc, column_name, value):  
+    cur = arcpy.UpdateCursor(fc)
+
+    for row in cur:
+        row.setValue(column_name, value)
+        cur.updateRow(row)
+
+def ProjectSF(state, infc, outfc):
+    os.chdir('C:/Users/Bougie/Desktop/Gibbs/Intact_land/SDSU_CLU_project/CLU Data/'+ state)
+    outfc_path = defineGDBpath(['intact_land',state]) + outfc
+
     # run project tool
-    arcpy.Project_management(infc, outfc, "PROJCS['Albers_Conical_Equal_Area',GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Albers'],PARAMETER['false_easting',0.0],PARAMETER['false_northing',0.0],PARAMETER['central_meridian',-96.0],PARAMETER['standard_parallel_1',29.5],PARAMETER['standard_parallel_2',45.5],PARAMETER['latitude_of_origin',23.0],UNIT['Meters',1.0]]", "", "PROJCS['NAD_1983_UTM_Zone_15N',GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Transverse_Mercator'],PARAMETER['False_Easting',500000.0],PARAMETER['False_Northing',0.0],PARAMETER['Central_Meridian',-93.0],PARAMETER['Scale_Factor',0.9996],PARAMETER['Latitude_Of_Origin',0.0],UNIT['Meter',1.0]]", "NO_PRESERVE_SHAPE", "", "NO_VERTICAL")
+    arcpy.Project_management(infc, outfc_path, "PROJCS['Albers_Conical_Equal_Area',GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Albers'],PARAMETER['false_easting',0.0],PARAMETER['false_northing',0.0],PARAMETER['central_meridian',-96.0],PARAMETER['standard_parallel_1',29.5],PARAMETER['standard_parallel_2',45.5],PARAMETER['latitude_of_origin',23.0],UNIT['Meters',1.0]]", "", "PROJCS['NAD_1983_UTM_Zone_15N',GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Transverse_Mercator'],PARAMETER['False_Easting',500000.0],PARAMETER['False_Northing',0.0],PARAMETER['Central_Meridian',-93.0],PARAMETER['Scale_Factor',0.9996],PARAMETER['Latitude_Of_Origin',0.0],UNIT['Meter',1.0]]", "NO_PRESERVE_SHAPE", "", "NO_VERTICAL")
 
 
 
@@ -242,54 +192,165 @@ def executeQuery(query):
 
 
 
-def main():
+def mainProjectSF():
 
-    query='''SELECT 
-             name
-             FROM 
-              metadata.metatable
-             where pub_start NOT IN (select max(pub_start) from metadata.metatable group by fips, EXTRACT(YEAR FROM pub_start))
-             order by fips, pub_start'''
-    print query
-    list_sf = g.fetchPG('intact_lands', query)
+    states = getDirectories()
+    for state in states:
+        if len(state) == 2 and state == 'mn':
 
-    for sf in list_sf:
-        fnf=(os.path.splitext(sf)[0]).split("_")
-        print fnf
-        fnf[3]='acea'
-        print fnf
-        outfc = '_'.join(fnf)
-        print outfc
-        try:
-            ProjectSF(['intact_land','ia5'], sf, outfc)
-        except:
-            print 'bad file:', sf
-            e = sys.exc_info()[1]
-            
-            query="INSERT INTO metadata.corrupt VALUES ('" + str(sf) + "','"+str(fnf[2])  + "','"+str(e.args[0]) + "')"
+            #variable with quotes
+            vwq = "'" + state + "'"
+
+            query="""SELECT 
+                    entire.name
+                    FROM  
+                    metadata.metatable as entire RIGHT OUTER JOIN(SELECT max(pub_start) as pub_start,fips FROM metadata.metatable GROUP BY fips, EXTRACT(YEAR FROM pub_start)) as subset 
+                    ON entire.pub_start = subset.pub_start AND entire.fips = subset.fips
+                    WHERE state = """ + vwq
+
             print query
-            executeQuery(query)
+            list_sf = g.fetchPG('intact_lands', query)
+            print list_sf
+            print len(list_sf)
+
+            for sf in list_sf:
+                fnf=(os.path.splitext(sf)[0]).split("_")
+                print fnf
+                fnf[3]='acea'
+                print fnf
+                outfc = '_'.join(fnf)
+                print outfc
+                try:
+                    ProjectSF(state, sf, outfc)
+                except:
+                    print 'bad file:', sf
+                    e = sys.exc_info()[1]
+                    
+                    query="INSERT INTO metadata.corrupt VALUES ('" + str(sf) + "','"+str(fnf[2])  + "','"+str(e.args[0]) + "')"
+                    print query
+                    executeQuery(query)
+
+
+
+def applyCond2FC(gdb_path, fc, expression, fc_out):
+ 
+    fc_in = defineGDBpath(gdb_path) + fc
+    print fc_in
+    out_path = defineGDBpath(gdb_path)
+
+    arcpy.FeatureClassToFeatureClass_conversion(fc_in, out_path, fc_out, expression) 
+
+
+
+def FieldExist(gdb_path, fc, fieldname):
+    arcpy.env.workspace = defineGDBpath(gdb_path)
+    fieldList = arcpy.ListFields(fc, fieldname)
+
+    fieldCount = len(fieldList)
+
+    if (fieldCount == 1):
+        if fieldname == "CLUCLSCD":
+            expression1 = "CLUCLSCD = 2"
+            fc_out1 = fc + '_clscd_2'
+            applyCond2FC(gdb_path, fc, expression1, fc_out1)
+            expression2 = "CLUCLSCD <> 2"
+            fc_out2 = fc + '_clscd_not2'
+            applyCond2FC(gdb_path, fc, expression2, fc_out2)
+        if fieldname == "CROPLND3CM":
+            expression1 = "CROPLND3CM = 1"
+            fc_out1 = fc + '_3cm_1'
+            applyCond2FC(gdb_path, fc, expression1, fc_out1)
+            expression2 = "CROPLND3CM <> 1"
+            fc_out2 = fc + '_3cm_not1'
+            applyCond2FC(gdb_path, fc, expression2, fc_out2)
 
 
 
 
 
 
-# def insertValuesPG(fnf):
+def mainSubsetFC():
+    states = getDirectories()
+    for state in states:
+        if len(state) == 2 and state == 'mn':
+            arcpy.env.workspace = defineGDBpath(['intact_land',state+'_merged'])
+            featureclasses = arcpy.ListFeatureClasses()
+            for fc in featureclasses:
+                print fc
+                fieldnames = ['CLUCLSCD','CROPLND3CM']
+                for field in fieldnames:
+                    FieldExist(['intact_land',state+'_merged'], fc, field)
 
 
-#     cur = conn.cursor()
-#     query="INSERT INTO metadata.metatable VALUES (" + str(fnf[2]) + " , '" + str(fnf[4]) + "' , '" + str(fnf[5])+ "')"
-#     print query
-#     cur.execute(query)
-#     conn.commit()
+
+def getDirectories():
+    os.chdir('C:/Users/Bougie/Desktop/Gibbs/Intact_land/SDSU_CLU_project/CLU Data/')
+    dirlist = glob.glob("*")
+    return dirlist
 
 
 
 
+
+def mainMergeFC():
+    #hard coded range of years of all datasets in clu
+    years = range(2003,2016)
+    states = getDirectories()
+    for state in states:
+        if len(state) == 2 and state == 'mn':
+            #only want to process the main directories with 2 letters that represnt the to letters of a state
+            print state
+
+            for year in years:
+                # mergeFC(['intact_land',state], '*_acea_'+str(year)+'*', state, year)
+                addFields(['intact_land',state+'_merged'], state, year)
+    
+
+
+
+
+
+
+def mergeSubsetsbyState():
+    subsetlist = ['_clscd_2','_clscd_not2','_3cm_1','_3cm_not1']
+    states = getDirectories()
+    for state in states:
+        if len(state) == 2 and state == 'mn':
+            for subset in subsetlist:
+                wc = state + '*' + subset
+                #only want to process the main directories with 2 letters that represnt the to letters of a state
+                print wc  
+                arcpy.env.workspace = defineGDBpath(['intact_land','merged'])
+                featureclasses = arcpy.ListFeatureClasses(wc)
+                print featureclasses
+                arcpy.Merge_management(featureclasses, state+subset)
 
 
 
 ######################  call functions  ###############################################################
-# createMetaTable('ia')
-main()
+
+
+#__________create the metatable for all states
+# createMetaTable()
+
+
+#__________reproject sf and store as fc in geodatabase
+# mainProjectSF()
+
+
+
+#________  create the base merged dataset by state and year _____________________________________________________________
+mainMergeFC()
+
+
+
+#_________  create the merged subset feature classes  ____________________________________
+# mainSubsetFC()
+
+
+# mergeSubsetsbyState()
+
+
+
+
+
