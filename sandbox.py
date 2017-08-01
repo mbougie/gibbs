6ca676c7d086b61d6652e86e80ec8a10505ec589
Matt Bougie <mbougie@wisc.edu>
2017-08-01 14:54:06 -0500

    still working on dir structure

---
+++ sandbox.py
@@ -0,0 +1,49 @@
+from sqlalchemy import create_engine
+import numpy as np, sys, os
+# from osgeo import gdal
+# from osgeo.gdalconst import *
+import pandas as pd
+import collections
+from collections import namedtuple
+# import openpyxl
+import arcpy
+from arcpy import env
+from arcpy.sa import *
+import glob
+import psycopg2
+import general as gen
+
+'''######## DEFINE THESE EACH TIME ##########'''
+#NOTE: need to declare if want to process ytc or yfc
+yxc = 'yfc'
+
+#the associated mtr value qwith the yxc
+yxc_mtr = {'ytc':'3', 'yfc':'4'}
+
+#Note: need to change this each time on different machine
+case=['Bougie','Gibbs']
+
+#import extension
+arcpy.CheckOutExtension("Spatial")
+
+
+
+###################  declare functions  #######################################################
+def defineGDBpath(arg_list):
+    gdb_path = 'C:/Users/'+case[0]+'/Desktop/'+case[1]+'/arcgis/geodatabases/'+arg_list[0]+'/'+arg_list[1]+'.gdb/'
+    print 'gdb path: ', gdb_path 
+    return gdb_path 
+
+
+
+
+
+
+# gen.addGDBTable2postgres(['refinement','refinement'],'*','refinement')
+
+# gen.getPGColumnsList("'refinement'", "'counties_yfc_bfnc'", " , ")
+
+
+gen.transposeTable(['refinement','refinement'],'counties_yfc_years')
+
+
